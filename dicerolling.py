aa13bb0243247c4eea9c73170057fccb4b32a63a
N shiva krishna <77113270+shiva-4@users.noreply.github.com>
2021-02-23 22:11:30 +0530

    Add files via upload

---
+++ dicerolling.py
@@ -0,0 +1,51 @@
+import random
+opt='y'
+
+while(opt=='y'):
+    number=random.randint(1,6)
+    if number==1:
+        print("----------")
+        print("|        |")
+        print("|    0   |")
+        print("|        |")
+        print("----------")
+    if number==2:
+        print("----------")
+        print("|        |")
+        print("| 0    0 |")
+        print("|        |")
+        print("----------")
+    if number==3:
+        print("----------")
+        print("|   0    |")
+        print("|        |")
+        print("| 0    0 |")
+        print("----------")
+    if number==4:
+        print("----------")
+        print("|        |")
+        print("| 0  0   |")
+        print("| 0  0   |")
+        print("----------")
+    if number==5:
+        print("----------")
+        print("| 0   0  |")
+        print("|   0    |")
+        print("| 0   0  |")
+        print("----------")
+    if number==1:
+        print("----------")
+        print("| 0  0   |")
+        print("| 0  0   |")
+        print("| 0  0   |")
+        print("----------")
+    opt=input("y or n")
+   
+
+
+        
+        
+
+
+
+
